cf33deede5111c3d705b6c8534a629172beb8557
acxanime <acxanime07@gmail.com>
2025-08-25 19:54:35 +0530

    Update commands.py

--- TechifyBots/commands.py
+++ TechifyBots/commands.py
@@ -94,7 +94,7 @@ async def approve_new(client, m):
         try:
             await client.send_message(
                 m.from_user.id,
-                f"{m.from_user.mention},\n\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝗌𝗍 𝖳𝗈 𝖩𝗈𝗂𝗇 {m.chat.title} 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝖼𝖼𝖾𝗉𝗍𝖾𝖽."
+                f"{m.from_user.mention},\n\n<b>ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ{m.chat.title} ᴀꜱ ʙᴇᴇɴ ᴀᴄᴄᴇᴘᴛᴇᴅ</b>."
             )
         except:
             pass
